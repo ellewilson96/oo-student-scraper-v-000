e09abf5e6828d2c9fa94cc1e7fd9e4685eb5275d
Elle Wilson <eleanorwilsonn@gmail.com>
2018-03-15 15:38:30 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,7 +21,7 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
   student = {}
-  profile_page = Nokogiri::HTML(open(profile_slug))
+  profile_page = Nokogiri::HTML(open(profile_url))
 
   end
 
